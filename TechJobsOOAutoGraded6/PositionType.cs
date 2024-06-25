e142a5c4732ecf33cf110eff125ee88030da7f98
jacxd64 <70988180+jacxd64@users.noreply.github.com>
2024-06-24 22:25:44 -0500

    TestTask 2 tests passed

--- TechJobsOOAutoGraded6/PositionType.cs
+++ TechJobsOOAutoGraded6/PositionType.cs
@@ -19,6 +19,21 @@ namespace TechJobsOOAutoGraded6
         }
 
         // TODO: Task 2: Add custom Equals(), GetHashCode(), and ToString() methods.
+        public override bool Equals(object obj)
+        {
+            return obj is Employer employer &&
+                   Id == employer.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
     }
 }
 
